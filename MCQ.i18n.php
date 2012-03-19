783bad46f9f025e995431fe4cfa94b5bcfb5ff9f
nischay nahata <nischayn22@gmail.com>
2012-03-19 17:56:39 +0530

    modified message

--- MCQ.i18n.php
+++ MCQ.i18n.php
@@ -13,17 +13,19 @@ $messages = array();
  */
 $messages['en'] = array(
 
-	'MCQcaptcha-desc' => 'MCQ CAPTCHA generator for Confirm Edit',
-	'MCQcaptcha-addurl' => 'Your edit includes new external links.
+	'mcqcaptcha-desc' => 'MCQ CAPTCHA generator for Confirm Edit',
+	'mcqcaptcha-addurl' => 'Your edit includes new external links.
 To help protect against automated spam, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
-	'MCQcaptcha-badlogin' => 'To help protect against automated password cracking, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
-	'MCQcaptcha-createaccount' => 'To help protect against automated account creation, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
-	'MCQcaptcha-create' => 'To create the page, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
-	'MCQcaptcha-edit' => 'To edit this page, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
-	'MCQcaptcha-sendemail' => 'To help protect against automated spamming, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
+	'mcqcaptcha-badlogin' => 'To help protect against automated password cracking, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
+	'mcqcaptcha-createaccount' => 'To help protect against automated account creation, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
+	'mcqcaptcha-create' => 'To create the page, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
+	'mcqcaptcha-edit' => 'To edit this page, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
+	'mcqcaptcha-sendemail' => 'To help protect against automated spamming, please answer the question that appears below ([[Special:Captcha/help|more info]]):',
 	
 	'mcqcaptchahelp-text' => 'Answer the Question using Common Sense',
-	
+	/** 
+	*update the question number in the MCQ.class.php when adding new questions
+	*/
 	'question1' => 'Which of the following is an Animal 1)Dog 2)Carrot 3)Bridge 4)Ship',
 	'answer1' => '1',
 	'question2' => 'We pray to 1)Time 1)God 3)Lion 4)Tomato',
